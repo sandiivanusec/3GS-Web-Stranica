ce1e4850b5a9235c4e11509b42ac49cbadfbbdd3
Sandi <s.ivanusec@gmail.com>
2023-07-02 13:48:11 +0100

    navbar fixes

--- script.js
+++ script.js
@@ -5,34 +5,40 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const ec_i_dropdown = document.getElementsByClassName("ec_i_dropdown")[0];
 const ec_dropdown_linkovi = document.getElementsByClassName("ec_dropdown_linkovi")[0];
 
+const energetskiCertifikati = document.getElementsByClassName("EnergetskiCertifikati")[0];
 
 
+function isMobile () {
+ return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 
-//Za pokrenuti drugi dropdown
-//ec_i_dropdown.addEventListener("mouseover", () => {
+if(isMobile()) {
 
-const energetskiCertifikati = document.getElementsByClassName("EnergetskiCertifikati")[0];
+  energetskiCertifikati.addEventListener("click", () => {
+    ec_dropdown_linkovi.classList.toggle("active");
+  });
 
-energetskiCertifikati.addEventListener("mouseover", () => {
-  ec_dropdown_linkovi.classList.add("active");
-  //ec_dropdown_linkovi.classList.toggle("active");
-});
-energetskiCertifikati.addEventListener("mouseout", () => {
-  ec_dropdown_linkovi.classList.remove("active");
-});
+} else {
 
-ec_dropdown_linkovi.addEventListener("mouseover", () => {
-  ec_dropdown_linkovi.classList.add("active");
-  //ec_dropdown_linkovi.classList.toggle("active");
-});
-ec_dropdown_linkovi.addEventListener("mouseout", () => {
-  ec_dropdown_linkovi.classList.remove("active");
-  //ec_dropdown_linkovi.classList.toggle("active");
-});
+  energetskiCertifikati.addEventListener("mouseover", () => {
+    ec_dropdown_linkovi.classList.add("active");
+  });
+  energetskiCertifikati.addEventListener("mouseout", () => {
+    ec_dropdown_linkovi.classList.remove("active");
+  });
+  
+  ec_dropdown_linkovi.addEventListener("mouseover", () => {
+    ec_dropdown_linkovi.classList.add("active");
+  });
+  ec_dropdown_linkovi.addEventListener("mouseout", () => {
+    ec_dropdown_linkovi.classList.remove("active");
+  });
+
+}
 
 
 
